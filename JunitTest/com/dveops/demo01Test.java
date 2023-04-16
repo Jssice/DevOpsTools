8fc1a25e6c47920c0cd8d570d868c010a9fba9de
Jssice <jiaxiaobin@hotmail.com>
2023-04-16 20:56:52 +0930

    inital pg

---
+++ JunitTest/com/dveops/demo01Test.java
@@ -0,0 +1,31 @@
+package com.dveops;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class demo01Test {
+
+	@Test
+	public void testSum() {
+//		fail("Not yet implemented");
+		assertEquals("125", new demo01().sum("5", 2,4,6));
+	}
+	@Test
+	public void testSum02() {
+		assertEquals("127", new demo01().sum("7",2,4,6));
+	}
+	@Test
+	public void testSum03() {
+		assertEquals("135", new demo01().sum("5", 2,4,7));
+	}
+	@Test
+	public void testSum04() {
+		assertEquals("125", new demo01().sum("7",2,4,6));
+	}
+	@Test
+	public void testSum05() {
+		assertEquals("135", new demo01().sum("5",2,4,6));
+	}
+
+}
